310170e6866e0b5395a965794ffd709219cd3b90
Sean Maxwell <sa.maxwell@yahoo.com>
2021-09-12 11:19:04 -0500

    Refactored to be multi-project by chapter

---
+++ Chapter5/src/main/java/com/myorg/CdkApp.java
@@ -0,0 +1,40 @@
+package com.myorg;
+
+import dev.stratospheric.cdk.SpringBootApplicationStack;
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class CdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        String accountId = (String) app
+                .getNode()
+                .tryGetContext("accountId");
+        Objects.requireNonNull(accountId, "context variable 'accountId' must not be null");
+
+        String region = (String) app
+                .getNode()
+                .tryGetContext("region");
+        Objects.requireNonNull(region, "context variable 'region' must not be null");
+
+        new SpringBootApplicationStack(
+                app,
+                "SpringBootApplication",
+                makeEnv(accountId, region),
+                "docker.io/seanimus/todo-app-v1:latest");
+
+        app.synth();
+    }
+
+    static Environment makeEnv(String account, String region) {
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
+    }
+}
