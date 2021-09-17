de62ab4e8bf6f37ebe44e0ecbf4c5e8909cca47a
Sean Maxwell <sa.maxwell@yahoo.com>
2021-09-17 17:45:55 -0500

    cdk-practice/cdk-workshop::updated stack to lambda+api gateway

--- cdk-practice/cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
+++ cdk-practice/cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -4,6 +4,10 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
@@ -16,6 +20,16 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
+        // define a new lambda resource
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
+                .build();
+
+        LambdaRestApi.Builder.create(this, "Endpoint")
+                .handler(hello)
+                .build();
 
     }
 }
